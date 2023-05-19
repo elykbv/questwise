51b47b33a957fcf685281d95369f0e02df163840
elykbv <elii.yakubov@gmail.com>
2023-05-19 09:00:16 -0400

    updated url param variable

--- src/pages/i/[id].tsx
+++ src/pages/i/[id].tsx
@@ -1,16 +1,31 @@
+import { DayCard, Layout } from '@/components'
 import { api } from '@/utils/api'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const Itinerary: NextPage = () => {
     const router = useRouter()
-    const { data, isLoading } = api.itinerary.getById.useQuery({
+
+    const { data, isLoading, isError } = api.itinerary.getById.useQuery({
         id: router.query.id as string,
     })
 
-    console.log(data)
+    console.log({ data })
 
-    return <div>{router.query.id}</div>
+    if (!data) return <div>Loading</div>
+
+    if (isError) return <div>Something went wrong</div>
+
+    return (
+        <Layout>
+            <div className="flex">
+                {data.days.map((day, index) => (
+                    <DayCard key={index} />
+                ))}
+            </div>
+        </Layout>
+    )
 }
 
 export default Itinerary
